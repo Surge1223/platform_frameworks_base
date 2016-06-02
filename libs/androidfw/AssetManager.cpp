e9676e5ac0c7e98c3e03c4617a49a3a6c925947d
Mårten Kongstad <marten.kongstad@sonymobile.com>
2016-06-02 09:35:31 +0200

    OMS: integrate OverlayManagerService into framework
    Hand over ownership of overlays to OverlayManagerService.
    
    Changes to a package's overlays are propagated using the activity life
    cycle. Affected activities will be recreated as needed. This provides a
    well-defined point to modify an application's assets while the
    application is paused.
    
    Consolidate how overlays targeting the system and overlays targeting
    regular applications are handled. Previously, system overlays were
    handled as a special case. Now, everything is handled identically. As a
    side effect, the call to idmap --scan during Zygote boot has become
    obsolete and is removed.
    
    Deprecate and remove use of SCAN_TRUSTED_OVERLAY from
    PackageManagerService. Previously, the flag was used to restrict what
    paths overlays were allowed to be installed in. Now, overlay packages
    are first class packages and it is up to the OverlayManagerService to
    decide whether to use the overlay or not.
    
    Information on what overlays to use is recorded in
    ApplicationInfo.resourceDirs. The PackageManagerService is responsible
    for the creation of ApplicationInfo objects. The OverlayManagerService
    is responsible for informing the PackageManagerService in advance about
    what resourceDirs to use.
    
    When launching an application, the ApplicationInfo is already populated
    with up-to-date information about overlays.
    
    When enabling or disabling an overlay for a running application, the
    OverlayManagerService first notifies the OverlayManagerService about the
    updated resourceDirs. It then tells the ActivityManagerService to push
    the new ApplicationInfo object to the application's ActivityThread.
    Finally the application requests its ResourcesManager to create new
    ResourcesImpl objects based on the updated paths.
    
    Co-authored-by: Martin Wallgren <martin.wallgren@sonymobile.com>
    Signed-off-by: Zoran Jovanovic <zoran.jovanovic@sonymobile.com>
    Bug: 31052947
    Test: run tests from 'OMS: tests for OverlayManagerService'
    Change-Id: Idc96dae6fc075d5373aa055bbf50e919136d7353
    
    Conflicts:
    	cmds/idmap/scan.cpp
    	libs/androidfw/AssetManager.cpp

--- libs/androidfw/AssetManager.cpp
+++ libs/androidfw/AssetManager.cpp
@@ -210,15 +210,6 @@ bool AssetManager::addAssetPath(
         *cookie = static_cast<int32_t>(mAssetPaths.size());
     }
 
-#ifdef __ANDROID__
-    // Load overlays, if any
-    asset_path oap;
-    for (size_t idx = 0; mZipSet.getOverlay(ap.path, idx, &oap); idx++) {
-        oap.isSystemAsset = isSystemAsset;
-        mAssetPaths.add(oap);
-    }
-#endif
-
     if (mResources != NULL) {
         appendPathToResTable(ap, appAsLib);
     }
@@ -593,11 +584,6 @@ FileType AssetManager::getFileType(const char* fileName)
 }
 
 bool AssetManager::appendPathToResTable(const asset_path& ap, bool appAsLib) const {
-    // skip those ap's that correspond to system overlays
-    if (ap.isSystemOverlay) {
-        return true;
-    }
-
     Asset* ass = NULL;
     ResTable* sharedRes = NULL;
     bool shared = true;
@@ -639,14 +625,6 @@ bool AssetManager::appendPathToResTable(const asset_path& ap, bool appAsLib) con
                 ALOGV("Creating shared resources for %s", ap.path.string());
                 sharedRes = new ResTable();
                 sharedRes->add(ass, idmap, nextEntryIdx + 1, false);
-#ifdef __ANDROID__
-                const char* data = getenv("ANDROID_DATA");
-                LOG_ALWAYS_FATAL_IF(data == NULL, "ANDROID_DATA not set");
-                String8 overlaysListPath(data);
-                overlaysListPath.appendPath(kResourceCache);
-                overlaysListPath.appendPath("overlays.list");
-                addSystemOverlays(overlaysListPath.string(), ap.path, sharedRes, nextEntryIdx);
-#endif
                 sharedRes = const_cast<AssetManager*>(this)->
                     mZipSet.setZipResourceTable(ap.path, sharedRes);
             }
@@ -1937,20 +1915,6 @@ bool AssetManager::SharedZip::isUpToDate()
     return mModWhen == modWhen;
 }
 
-void AssetManager::SharedZip::addOverlay(const asset_path& ap)
-{
-    mOverlays.add(ap);
-}
-
-bool AssetManager::SharedZip::getOverlay(size_t idx, asset_path* out) const
-{
-    if (idx >= mOverlays.size()) {
-        return false;
-    }
-    *out = mOverlays[idx];
-    return true;
-}
-
 AssetManager::SharedZip::~SharedZip()
 {
     if (kIsDebug) {
@@ -2074,22 +2038,6 @@ bool AssetManager::ZipSet::isUpToDate()
         }
     }
     return true;
-}
-
-void AssetManager::ZipSet::addOverlay(const String8& path, const asset_path& overlay)
-{
-    int idx = getIndex(path);
-    sp<SharedZip> zip = mZipFile[idx];
-    zip->addOverlay(overlay);
-}
-
-bool AssetManager::ZipSet::getOverlay(const String8& path, size_t idx, asset_path* out) const
-{
-    sp<SharedZip> zip = SharedZip::get(path, false);
-    if (zip == NULL) {
-        return false;
-    }
-    return zip->getOverlay(idx, out);
 }
 
 /*
